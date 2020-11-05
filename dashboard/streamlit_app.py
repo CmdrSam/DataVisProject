5b89c8d026e5e5f5722c0de0d0d44a7213ece72c
Aditya Oke <okeaditya315@gmail.com>
2020-11-05 19:40:36 +0530

    adds new straemlit app

--- dashboard/streamlit_app.py
+++ dashboard/streamlit_app.py
@@ -11,72 +11,33 @@ import plotly.express as px
 import plotly.io as pio
 
 
-@st.cache(suppress_st_warning=True)
-def displayPlot(d):
-    fig = go.Figure(data=d, layout=layout)
-    # fig
-
 if __name__ == "__main__":
     # Use write for most of the text
     st.title('DataViz for DataViz')
     st.write("Starting Data science journey can be difficult, let us help you.")
 
-    survey_df = pd.read_csv("multipleChoiceResponses.csv") 
-    responses_df = survey_df[1:]
-    responses_df_orig = responses_df.copy()
+    exp = st.radio("How experienced are you ? ", ("No Idea", "Beginner", "Expert"), index=0)
 
-    responses_df = responses_df[~pd.isnull(responses_df['Q35_Part_1'])]
-    count_dict = {
-        'Self-taught' : (responses_df['Q35_Part_1'].astype(float)>0).sum(),
-        'Online courses (Coursera, Udemy, edX, etc.)' : (responses_df['Q35_Part_2'].astype(float)>0).sum(),
-        'Work' : (responses_df['Q35_Part_3'].astype(float)>0).sum(),
-        'University' : (responses_df['Q35_Part_4'].astype(float)>0).sum(),
-        'Kaggle competitions' : (responses_df['Q35_Part_5'].astype(float)>0).sum(),
-        'Other' : (responses_df['Q35_Part_6'].astype(float)>0).sum()
-    }
+    if exp == "No Idea":
+        st.write("Hey have a look at Data Science")
 
-    cnt_srs = pd.Series(count_dict)
+    elif exp == "Beginner":
+        st.write("Let us help you to get started in Data Science")
 
-    trace = go.Bar(
-        x=cnt_srs.index,
-        y=cnt_srs.values,
-        marker=dict(
-            color='#ffa600'
-        ),
-    )
+    elif exp == "Expert":
+        st.write("Hey, you look expreinced, let us explore some new concepts")
+        # st.image("../visualizations/newplot (5).png")
+        # st.image("../visualizations/newplot (6).png")
+        # st.image("../visualizations/newplot (7).png")
 
-    layout = go.Layout(
-        title='Number of respondents for each learning category'
-    )
-
-    data = [trace]
-    displayPlot(data)
-
-    st.write("This should take less time")
-
-    option = st.sidebar.selectbox(
-        'Which number do you like best?',
-        df['first column'])
-
-    'You selected:', option
-
-            # To make table
-    # st.write(pd.DataFrame({
-    #     'first column': [1, 2, 3, 4],
-    #     'second column': [10, 20, 30, 40]
-    # }))
+    # if st.button('Say hello'):
+    #     st.write('Why hello there')
+        # st.image("../visualizations/newplot (1).png")
+        # st.image("../visualizations/newplot (2).png")
+        # st.image("../visualizations/newplot (3).png")
+        # st.image("../visualizations/newplot (4).png")
 
 
-    # chart_data = pd.DataFrame(
-    #      np.random.randn(20, 3),
-    #      columns=['a', 'b', 'c'])
-
-    # st.line_chart(chart_data)
-
-
-            #To get map data don't know how to use
-    # map_data = pd.DataFrame(
-    #     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    #     columns=['lat', 'lon'])
-
-    # st.map(map_data)
+    # else:
+    #     st.write('Goodbye')    
+    
