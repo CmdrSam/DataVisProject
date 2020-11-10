5a2bc3a491ae2b9016a2299783b4bc4db7be49b2
Aditya Oke <okeaditya315@gmail.com>
2020-11-10 17:30:47 +0530

    adds plots

--- dashboard/streamlit_app.py
+++ dashboard/streamlit_app.py
@@ -14,7 +14,7 @@ import plotly.io as pio
 if __name__ == "__main__":
     # Use write for most of the text
     st.title('RoadMap for Data Science')
-    # st.balloons()
+    st.balloons()
 
     st.subheader("Starting Data science journey can be difficult, let us help you.")
 
@@ -26,17 +26,108 @@ if __name__ == "__main__":
                                options=["No Idea", "Beginner", "Expert"], index=0)
 
     edu = st.sidebar.select_slider("Select your education",
-                            options=["High School", "Undergrad", "Post-Grad", "PhD"])
+                                    options=["High School", "Undergrad", "PostGrad", "PhD"])
 
-    if exp == "No Idea":
-        st.write("Hey have a look at Data Science")
+    if exp == "No Idea" and edu == "High School":
+        st.write("So you don't have much idea about data science")
+        st.write("You are in High school, super enthusisastic about data science")
 
-    elif exp == "Beginner":
+        st.image("../visualizations/images/MostUsedPlatform.png")
+        st.image("../visualizations/images/language.png")
+        st.image("../visualizations/images/degree.png")
+        st.image("../visualizations/images/emp_industry.png")
+        st.image("../visualizations/images/hardware.png")
+        st.image("../visualizations/images/how_learnt.png")
+
+    elif exp == "No Idea" and edu == "Undergrad":
+        st.write("So you don't have much idea about data science")
+        st.write("You are in Undergrad, very eager to know about data science")
+        st.image("../visualizations/images/language.png")
+        st.image("../visualizations/images/degree.png")
+        st.image("../visualizations/images/emp_industry.png")
+        st.image("../visualizations/images/hardware.png")
+        st.image("../visualizations/images/how_learnt.png")
+
+    elif exp == "No Idea" and edu == "PostGrad":
+        st.write("So you don't have much idea about data science")
+        st.image("../visualizations/images/language.png")
+        st.image("../visualizations/images/degree.png")
+        st.image("../visualizations/images/emp_industry.png")
+        st.image("../visualizations/images/hardware.png")
+        st.image("../visualizations/images/how_learnt.png")
+
+    elif exp == "No Idea" and edu == "PhD":
+        st.write("So you don't have much idea about data science")
+        st.image("../visualizations/images/language.png")
+        st.image("../visualizations/images/averagePapersPublisedPerDay.png")
+        st.image("../visualizations/images/degree.png")
+        st.image("../visualizations/images/emp_industry.png")
+        st.image("../visualizations/images/growth_ml.png")
+        st.image("../visualizations/images/hardware.png")
+        st.image("../visualizations/images/how_learnt.png")
+        st.image("../visualizations/images/TopPopularAuthors.png")
+
+    elif exp == "Beginner" and edu == "High School":
         st.write("Let us help you to get started in Data Science")
+        st.write("You are in High school, super enthusisastic about data science")
+        st.image("../visualizations/images/employment.png")
+        st.image("../visualizations/images/time_spent.png")
 
-    elif exp == "Expert":
+    elif exp == "Beginner" and edu == "Undergrad":
+        st.write("Hey have a look at Data Science")
+        st.write("You are in Undergrad, very eager to know about data science")
+        st.image("../visualizations/images/ContributionByDegree.png")
+        st.image("../visualizations/images/employment.png")
+        st.image("../visualizations/images/time_spent.png")
+
+    elif exp == "Beginner" and edu == "PostGrad":
+        st.write("Hey have a look at Data Science")
+        st.image("../visualizations/images/algos.png")
+        st.image("../visualizations/images/ContributionByDegree.png")
+        st.image("../visualizations/images/employment.png")
+        st.image("../visualizations/images/time_spent.png")
+
+    elif exp == "Beginner" and edu == "PhD":
+        st.write("Hey have a look at Data Science")
+        st.image("../visualizations/images/algos.png")
+        st.image("../visualizations/images/time_spent.png")
+        st.image("../visualizations/images/averagePapersPublisedPerDay.png")
+        st.image("../visualizations/images/ContributionByDegree.png")
+        st.image("../visualizations/images/growth_ml.png")
+        st.image("../visualizations/images/TopPopularAuthors.png")
+
+    elif exp == "Expert" and edu == "High School":
+        interest = st.sidebar.radio("What's your Key Interest", ('Computer Vision', 'NLP', 'Data Science'))
+        st.write("You are in High school, super enthusisastic about data science")
+        st.image("../visualizations/images/algos.png")
+        st.image("../visualizations/images/methods.png")
+        st.image("../visualizations/images/ml_method.png")
+        st.image("../visualizations/images/prod.png")
+
+    elif exp == "Expert" and edu == "Undergrad":
         interest = st.sidebar.radio("What's your Key Interest", ('Computer Vision', 'NLP', 'Data Science'))
         st.write("Hey, you look expreinced, let us explore some new concepts")
+        st.image("../visualizations/images/ContributionByDegree.png")
+        st.image("../visualizations/images/methods.png")
+        st.image("../visualizations/images/ml_method.png")
+        st.image("../visualizations/images/prod.png")
 
+    elif exp == "Expert" and edu == "PostGrad":
+        interest = st.sidebar.radio("What's your Key Interest", ('Computer Vision', 'NLP', 'Data Science'))
+        st.write("Hey, you look expreinced, let us explore some new concepts")
+        st.image("../visualizations/images/algos.png")
+        st.image("../visualizations/images/ContributionByDegree.png")
+        st.image("../visualizations/images/methods.png")
+        st.image("../visualizations/images/ml_method.png")
+        st.image("../visualizations/images/prod.png")
 
-
+    elif exp == "Expert" and edu == "PhD":
+        interest = st.sidebar.radio("What's your Key Interest", ('Computer Vision', 'NLP', 'Data Science'))
+        st.write("Hey, you look expreinced, let us explore some new concepts")
+        st.image("../visualizations/images/averagePapersPublisedPerDay.png")
+        st.image("../visualizations/images/ContributionByDegree.png")
+        st.image("../visualizations/images/growth_ml.png")
+        st.image("../visualizations/images/methods.png")
+        st.image("../visualizations/images/ml_method.png")
+        st.image("../visualizations/images/prod.png")
+        st.image("../visualizations/images/TopPopularAuthors.png")
